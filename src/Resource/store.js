36932c329a38b14a4a3df3812bf782bc85eea683
ripaelit <ripaelit1111@gmail.com>
2023-04-16 14:16:19 +0800

    modifying store.js

--- src/Resource/store.js
+++ src/Resource/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
-
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import * as Sentry from '@sentry/react';
 import createSentryMiddleware from 'redux-sentry-middleware';
@@ -16,10 +16,11 @@ const rootReducer = combineReducers({
 
 const sentryEnhancedMiddlewares = applyMiddleware(thunk, createSentryMiddleware(Sentry, {}));
 
-const enableDevTools = process.env.NODE_ENV !== 'production' || process.env.REACT_APP_DEVTOOLS === 'true';
+// const enableDevTools = process.env.NODE_ENV !== 'production' || process.env.REACT_APP_DEVTOOLS === 'true';
 
 const reduxDevToolsEnhancedMiddlewares = sentryEnhancedMiddlewares;
 
-const store = createStore(rootReducer, reduxDevToolsEnhancedMiddlewares);
+// const store = createStore(rootReducer, reduxDevToolsEnhancedMiddlewares);
+const store = configureStore({ reducer: rootReducer });
 
 export default store;
