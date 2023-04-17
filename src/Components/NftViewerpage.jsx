bcd2fedd26cd3e0de87f2e5a5c3cc99fbda02cb2
ripaelit <ripaelit1111@gmail.com>
2023-04-17 16:55:42 +0200

    Update NftViewerpage.jsx

--- src/Components/NftViewerpage.jsx
+++ src/Components/NftViewerpage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { getTokens } from './Wallet/wallet'
+import { getTokens } from '../Resource/helpers/wallet'
 
 const NftViewerpage = () => {
   const [tokens, setTokens] = useState([])
@@ -11,26 +11,26 @@ const NftViewerpage = () => {
   }, [])
 
   return (
-    <div className='flex items-center justify-center flex-col h-screen'>
+    <div className={tokens.length > 18 ? 'flex items-center justify-center flex-col h-auto pt-32 pb-32' : 'flex items-center justify-center flex-col h-screen pt-32 pb-32'}>
       <div>
-        <h2 className='text-3xl font-bold text-center py-5'>Your Monster NFTs </h2>
+        <h2 className='text-3xl font-bold text-center py-9'>Your Monster NFTs </h2>
       </div>
       {/* Wallet Nfts */}
-      <div className='px-4 md:px-0 grid grid-cols-3 md:grid-cols-5 gap-6 items-center justify-center -z-10'>
+      <div className='h-full px-4 md:px-0 grid grid-cols-3 md:grid-cols-5 xl:grid-cols-6 gap-6 items-center justify-center -z-10'>
         {
           // walletNfts.map((wnft) => (
           tokens.length > 0 ? tokens.map((tokenId) => (
             <div key={tokenId} className='relative'>
               <img
-                src={`https://ipfs.io/ipfs/bafybeidlkqhddsjrdue7y3dy27pu5d7ydyemcls4z24szlyik3we7vqvam/${tokenId}.png`} 
+                src={`https://crazymonsters.s3.amazonaws.com/CrazyMonsterImages/${tokenId}.png`} 
                 alt="nft"
-                className='w-48 rounded-xl shadow-xl -z-10'
+                className='w-32 rounded-xl shadow-xl -z-10'
               />
               <span className='absolute top-0 px-3 py-1 bg-slate-700 bg-opacity-70 rounded-xl'>{tokenId}</span>
             </div>
           ))
           :
-          <div>Loading...</div>
+          <div  className='h-screen'>Loading...</div>
         }
       </div>
     </div>
